d0fa53defddd0368419c6e2e094fd882a4ac06af
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2022-12-07 14:56:32 +0000

    add data preparation script

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,13 @@
+# Analysis of WEEE dataset
+
+## Setup Repository
+
+### Requirements
+
+- Python 3
+- [Pipenv](https://pipenv.pypa.io/en/latest/)
+
+### Install dependencies
+```bash
+pipenv install
+```
---
+++ .ipynb_checkpoints/prepare-checkpoint.sh
@@ -0,0 +1,44 @@
+#!/bin/sh 
+
+DATASET_LINK="https://zenodo.org/record/6420886/files/dataset.zip?download=1"
+
+function install_aria2() {
+    # if apt avaliable use it 
+    if [ -x "$(command -v apt)" ]; then
+        sudo apt install aria2
+    # if pacman avaliable use it
+    elif [ -x "$(command -v pacman)" ]; then
+        sudo pacman -S aria2
+    # if dnf avaliable use it
+    elif [ -x "$(command -v dnf)" ]; then
+        sudo dnf install aria2
+    fi
+}
+
+if [ ! -f /usr/bin/aria2c ]; then
+    printf "aria2 not found, install it now? [Y/n] "
+    read -r answer
+    if [ "$answer" = "Y" ] || [ "$answer" = "y" ] || [ "$answer" = "" ]; then
+        install_aria2
+    else
+        exit 1
+    fi
+fi
+
+# if data/v1 already exists exit 
+if [ -d data/v1 ]; then
+    printf "data/v1 already exists, do you want to overwrite it? [Y/n] "
+    read -r answer
+    if [ "$answer" = "Y" ] || [ "$answer" = "y" ] || [ "$answer" = "" ]; then
+        rm -rf data/v1
+    else
+        exit 1
+    fi
+fi
+
+mkdir data
+aria2c DATASET_LINK -o data/dataset.zip
+unzip data/dataset.zip -d data/temp
+mv data/temp/dataset data/v1
+rm -rf data/temp
+
