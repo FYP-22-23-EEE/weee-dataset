f7d47e0ae45f459f7bcace088b9098cf2103ed3d
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2022-12-08 01:43:24 +0000

    add github-ssh

---
+++ github-ssh.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+EMAIL="thilina.18@cse.mrt.ac.lk"
+NAME="Thilina Lakshan"
+
+# ask to change the default email and name 
+read -p "Enter your email: " EMAIL 
+read -p "Enter your name: " NAME
+
+git config --global user.name "$NAME"
+git config --global user.email "$EMAIL"
+
+
+mkdir ~/.ssh 
+cd ~/.ssh 
+ssh-keygen -o -t rsa -C "$EMAIL"
+
+# add config file 
+touch config
+tee config <<EOF
+Host *
+    AddKeysToAgent yes
+    UseKeychain yes
+    IdentityFile ~/.ssh/id_rsa 
+EOF
+
+# add key to ssh-agent
+ssh-add -K ~/.ssh/id_rsa
+
+# print public key
+echo "---------------------------------------------"
+cat ~/.ssh/id_rsa.pub
+echo "---------------------------------------------"
+
